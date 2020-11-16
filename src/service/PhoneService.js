abda20f0e2d835250d1e8adf124e59abfa2675f7
Aitor <aitorulz1@gmail.com>
2020-11-16 17:08:12 +0100

    Setting context, types and App.js

--- src/service/PhoneService.js
+++ src/service/PhoneService.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const http = axios.create({
+const phones = axios.create({
     baseURL: process.env.BASE_URL || 'http://localhost:4000'
 })
-
-const phones = () => http.get('phones')
 
 export default {
     phones
