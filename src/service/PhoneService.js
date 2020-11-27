3164f8a14f797d33dd37e6d1420d296ac138b316
Aitor <aitorulz1@gmail.com>
2020-11-27 14:42:00 +0100

    updated

--- src/service/PhoneService.js
+++ src/service/PhoneService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const phoneService = axios.create({
-    baseURL: process.env.BASE_URL || 4000
+    baseURL: process.env.BASE_URL || 'http://localhost:4000/'
 })
 
 export default phoneService
