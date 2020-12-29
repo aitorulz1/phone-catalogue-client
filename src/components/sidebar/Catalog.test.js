7d138819b1f3a3f8767f3e1f7e5cd6a42e1927d4
Aitor <aitorulz1@gmail.com>
2020-12-29 11:21:37 +0100

    Basic config of our testing

---
+++ src/components/sidebar/Catalog.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Catalog from './Catalog';
+
+test('should appear a function called obtenerPhonesAction', () => {
+    expect(obtenerPhonesAction())
+});
